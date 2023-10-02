c35c2533af877fe5e3b5f8b45455dcedd4b61786
minth <minthutatun@gmail.com>
2023-10-02 22:01:15 +0630

    popup for premium shops to digital ocean

--- app/Helpers/custom.php
+++ app/Helpers/custom.php
@@ -2,18 +2,20 @@
 
 use Illuminate\Support\Facades\Storage;
 
-function filedopath($uri){
-    if(env('USE_DO') == 'true'){
-        return env('DIGITALOCEAN_SPACES_ENDPOINT').'/prod'.$uri;
+function filedopath($uri)
+{
+    if (env('USE_DO') == 'true') {
+        return env('DIGITALOCEAN_SPACES_ENDPOINT') . '/prod' . $uri;
 
-    }else{
-        return url('/images'.$uri);
+    } else {
+        return url('/images' . $uri);
     }
 }
-function dofile_exists($uri){
-    if(env('USE_DO')=='true'){
-        return Storage::disk('digitalocean')->exists('/prod'.$uri);
-    }else{
+function dofile_exists($uri)
+{
+    if (env('USE_DO') == 'true') {
+        return Storage::disk('digitalocean')->exists('/prod' . $uri);
+    } else {
         return Storage::disk('public_image')->exists($uri);
     }
 }
--- app/Http/Controllers/Shopowner/PopupAdsController.php
+++ app/Http/Controllers/Shopowner/PopupAdsController.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers\ShopOwner;
 
-use File;
-use App\Models\MainPopup;
-use App\Models\ShopOwner;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Manager;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\Trait\UserRole;
+use App\Models\MainPopup;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\View\View;
 
 class PopupAdsController extends Controller
@@ -33,41 +30,71 @@ class PopupAdsController extends Controller
         $this->validate($request, [
             'video' => 'required|file|mimetypes:video/mp4',
         ]);
+
+        $useDigitalOcean = env('USE_DO') == 'true';
+
         $checkExist = MainPopup::where('shop_id', $current_shop)->first();
-        // dd($checkExist);
+
         if ($checkExist) {
-            MainPopup::where('shop_id', $current_shop)->delete();
-            File::delete($checkExist->video_path);
+            // Delete existing file and database record
+            if ($useDigitalOcean) {
+                Storage::disk('digitalocean')->delete($checkExist->video_path);
+            } else {
+                File::delete(public_path($checkExist->video_path));
+            }
+            $checkExist->delete();
         }
+
         $file = $request->file('video');
         $fileName = $request->video->getClientOriginalName();
-        $filePath = 'test/video/' . $fileName;
+        $filePath = $useDigitalOcean ? 'prod/videos/' . $fileName : 'images/videos/' . $fileName;
         $shop_id = $current_shop;
         $ad_title = $request->adTitle;
-        $isFileUploaded = $file->move(public_path('test/video/'), $fileName);;
 
-        // File URL to access the video in frontend
-        // $url = Storage::disk('public')->url($filePath);
-        // dd($fileName." ".$filePath." ".$isFileUploaded." ".$url." ".$shop_id);
-        if ($isFileUploaded) {
-            $video = new MainPopup();
-            $video->ad_title = $ad_title;
-            $video->video_name = $fileName;
-            $video->video_path = $filePath;
-            $video->shop_id = $shop_id;
-            $video->save();
-            return back()->with('success', 'Video has been successfully uploaded!');
+        // Move the file to the public folder
+        if ($useDigitalOcean) {
+            Storage::disk('digitalocean')->put($filePath, file_get_contents($file), 'public');
+        } else {
+            $file->move(public_path($useDigitalOcean ? 'prod/videos/' : 'images/videos/'), $fileName);
         }
 
-        return back()->with('error', 'Unexpected error occured');
+        // File URL to access the video in frontend
+        $url = $useDigitalOcean ? Storage::disk('digitalocean')->url($filePath) : asset($filePath);
+
+        $video = new MainPopup();
+        $video->ad_title = $ad_title;
+        $video->video_name = $fileName;
+        $video->video_path = $filePath;
+        $video->shop_id = $shop_id;
+        $video->save();
+
+        return back()->with('success', 'Video has been successfully uploaded!');
     }
 
     public function main_popup_delete(): RedirectResponse
     {
         $current_shop = $this->get_shopid();
         $video = MainPopup::where('shop_id', $current_shop)->first();
-        MainPopup::where('shop_id', $current_shop)->delete();
-        File::delete($video->video_path);
-        return back()->with('success', 'Video deleted successfully!');
+
+        if ($video) {
+
+            // Determine storage disk based on USE_DO
+            $useDigitalOcean = env('USE_DO') == 'true';
+
+            if ($useDigitalOcean) {
+                // Delete the file from DigitalOcean Spaces
+                Storage::disk('digitalocean')->delete($video->video_path);
+            } else {
+                // Delete from the public folder
+                File::delete(public_path($video->video_path));
+            }
+
+            $video->delete();
+
+            return back()->with('success', 'Video deleted successfully!');
+        }
+
+        return back()->with('error', 'No video found to delete.');
     }
+
 }
